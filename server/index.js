6a9c7e2eb5761b4e39fde6407f005a35723e8417
jon-zawada <jonathan_zawada@yahoo.com>
2024-03-03 13:00:52 -0800

    implement https/http server and redirecting

--- server/index.js
+++ server/index.js
@@ -1,20 +1,50 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
+const https = require("https");
+const http = require("http");
+const fs = require("fs");
 const app = express();
-const PORT = process.env.PORT || 5001;
+const HTTP_PORT = process.env.PORT || 80;
+const HTTPS_PORT = process.env.HTTPS_PORT || 443;
 
 const clientBuild = path.join(__dirname, "..", "chester", "build");
 
-app.use(express.static(clientBuild));
+// SSL Certificate Configuration
+const privateKey = fs.readFileSync(path.join(__dirname, "..", "privatekey.pem"), "utf8");
+const certificate = fs.readFileSync(path.join(__dirname, "..", "certificate.pem"), "utf8");
+const ca = fs.readFileSync(path.join(__dirname, "..", "csr.pem"), "utf8");
 
-app.get('/alive', (req, res) => {
-  res.send('Server is alive!');
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+};
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+app.use((req, res, next) => {
+  if (req.secure || req.headers["x-forwarded-proto"] === "https") {
+    next();
+  } else {
+    res.redirect("https://" + req.headers.host + req.url);
+  }
 });
 
-app.get('*', (req, res) => {
+app.use(express.static(clientBuild));
+
+app.get("/alive", (req, res) => {
+  res.send("Server is alive!");
+});
+
+app.get("*", (req, res) => {
   res.sendFile(path.join(clientBuild, "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Portfolio listening on port ${PORT}`);
+httpServer.listen(HTTP_PORT, () => {
+  console.log(`Portfolio listening on ${HTTP_PORT} ---> mapped to 80`);
+});
+
+httpsServer.listen(HTTPS_PORT, () => {
+  console.log(`Portfolio listening on port ${HTTPS_PORT} ---> mapped to 443`);
 });
