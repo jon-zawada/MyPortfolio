c58de538ae7f85da8b2903663245ef813e03bc69
jon-zawada <jonathan_zawada@yahoo.com>
2024-03-04 01:47:04 -0800

    https with ports setup to be mapped

--- server/index.js
+++ server/index.js
@@ -1,9 +1,34 @@
 const express = require("express");
 const path = require('path');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const app = express();
-const PORT = process.env.PORT || 5001;
+const HTTP_PORT = process.env.PORT || 3000;
+const HTTPS_PORT = process.env.PORT || 3001;
 
 const clientBuild = path.join(__dirname, "..", "chester", "build");
+
+const privateKey = fs.readFileSync(path.join(__dirname, "..", "certs", "privatekey.pem"), "utf8");
+const certificate = fs.readFileSync(path.join(__dirname, "..", "certs", "certificate.pem"), "utf8");
+const ca = fs.readFileSync(path.join(__dirname, "..", "certs", "csr.pem"), "utf8");
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+}
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+app.use((req, res, next) => {
+  if (req.secure || req.headers["x-forwarded-proto"] === "https") {
+    next();
+  } else {
+    res.redirect("https://" + req.headers.host + req.url);
+  }
+});
 
 app.use(express.static(clientBuild));
 
@@ -15,6 +40,10 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(clientBuild, "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Portfolio listening on port ${PORT}`);
+httpServer.listen(HTTP_PORT, () => {
+  console.log(`Portfolio listening on ${HTTP_PORT} ---> mapped to 80`);
+});
+
+httpsServer.listen(HTTPS_PORT, () => {
+  console.log(`Portfolio listening on port ${HTTPS_PORT} ---> mapped to 443`);
 });
